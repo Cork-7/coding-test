1631c0118484cdcf7530b2a43baf08e00a6769fa
이승민 <iseungmin@SeungMin-MacBookPro.local>
2026-04-13 23:29:39 +0900

    Solve: Programmers Level 0 2 problems

---
+++ src/programmers/level_0/짝수와_홀수/Solution.java
@@ -0,0 +1,13 @@
+package programmers.level_0.짝수와_홀수;
+
+class Solution {
+    public String solution(int num) {
+        String answer = "";
+        if (num%2 == 0) {
+            answer = "Even";
+        } else {
+            answer = "Odd";
+        }
+        return answer;
+    }
+}
